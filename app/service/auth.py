7831eb1d032b43c5e934e30249282f8281709354
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-10-16 05:31:24 +0700

    Update auth.py

--- app/service/auth.py
+++ app/service/auth.py
@@ -4,19 +4,13 @@ import time
 
 from app.client.engsel import get_new_token
 
-
 class Auth:
     _instance_ = None
     _initialized_ = False
 
     api_key = ""
-
     refresh_tokens = []
-    # Format of refresh_tokens: [{"number": int, "refresh_token": str}]
-
     active_user = None
-    # Format of active_user: {"number": int, "tokens": {"refresh_token": str, "access_token": str, "id_token": str}}
-
     last_refresh_time = None
 
     def __new__(cls, *args, **kwargs):
@@ -29,11 +23,9 @@ class Auth:
             if os.path.exists("refresh-tokens.json"):
                 self.load_tokens()
             else:
-                # Create empty file
                 with open("refresh-tokens.json", "w", encoding="utf-8") as f:
                     json.dump([], f, indent=4)
 
-            # Select active user from file if available
             self.load_active_number()
 
             self.last_refresh_time = int(time.time())
@@ -47,7 +39,6 @@ class Auth:
             if len(refresh_tokens) != 0:
                 self.refresh_tokens = []
 
-            # Validate and load tokens
             for rt in refresh_tokens:
                 if "number" in rt and "refresh_token" in rt:
                     self.refresh_tokens.append(rt)
@@ -55,7 +46,6 @@ class Auth:
                     print(f"Invalid token entry: {rt}")
 
     def add_refresh_token(self, number: int, refresh_token: str):
-        # Check if number already exist, if yes, replace it, if not append
         existing = next((rt for rt in self.refresh_tokens if rt["number"] == number), None)
         if existing:
             existing["refresh_token"] = refresh_token
@@ -65,22 +55,17 @@ class Auth:
                 "refresh_token": refresh_token
             })
 
-        # Save to file
         self.write_tokens_to_file()
 
-        # Set active user to newly added
         self.set_active_user(number)
 
     def remove_refresh_token(self, number: int):
         self.refresh_tokens = [rt for rt in self.refresh_tokens if rt["number"] != number]
 
-        # Save to file
         with open("refresh-tokens.json", "w", encoding="utf-8") as f:
             json.dump(self.refresh_tokens, f, indent=4)
 
-        # If the removed user was the active user, select a new active user if available
         if self.active_user and self.active_user["number"] == number:
-            # Select the first user as active user by default
             if len(self.refresh_tokens) != 0:
                 first_rt = self.refresh_tokens[0]
                 tokens = get_new_token(first_rt["refresh_token"])
@@ -91,7 +76,6 @@ class Auth:
                 self.active_user = None
 
     def set_active_user(self, number: int):
-        # Get refresh token for the number from refresh_tokens
         rt_entry = next((rt for rt in self.refresh_tokens if rt["number"] == number), None)
         if not rt_entry:
             print(f"No refresh token found for number: {number}")
@@ -109,13 +93,7 @@ class Auth:
             "tokens": tokens
         }
 
-        # Save active number to file
         self.write_active_number()
-
-        # Put the selected user to the top of the list
-        # self.refresh_tokens = [rt for rt in self.refresh_tokens if rt["number"] != number]
-        # self.refresh_tokens.insert(0, rt_entry)
-        # self.write_tokens_to_file()
 
     def renew_active_user_token(self):
         if self.active_user:
@@ -137,7 +115,6 @@ class Auth:
 
     def get_active_user(self):
         if not self.active_user:
-            # Choose the first user if available
             if len(self.refresh_tokens) != 0:
                 first_rt = self.refresh_tokens[0]
                 tokens = get_new_token(first_rt["refresh_token"])

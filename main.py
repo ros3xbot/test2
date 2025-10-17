837383cee5fb0df38f4681d75b9a6a6b58c71c23
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-10-17 18:17:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def git_pull_rebase():
         run_git()
 
     if result["status"] == "success":
-        print_panel("✅ Berhasil update aplikasi.", theme["border_success"])
+        print_panel("✅ Berhasil update aplikasi", theme["border_success"])
     elif result["status"] == "fail":
         print_panel(f"❌ Gagal menjalankan git pull --rebase:\n{result['error']}", theme["border_err"])
     else:
